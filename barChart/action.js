cb91b43f37c0be710c11cb513e956fa49020c30a
KED <kaipie3@gmail.com>
2020-10-22 09:43:49 -0600

    finished basic bar graph

--- barChart/action.js
+++ barChart/action.js
@@ -1,16 +1,51 @@
-const svg = d3.select("svg");
+const svg = d3.select("#viz")
+    .append("svg")
+    .attr("version", "1.1")
+    .attr("viewBox", "0 0 960 500")
+    .attr("preserveAspectRatio", "xMinYMin meet");
 
-const width = +svg.attr("width");
-const height = +svg.attr("height");
+// const width = +svg.attr("width");
+// const height = +svg.attr("height");
+
+const width = 960;
+const height = 500;
 
 const render = data => {
+    const margin = {top: 20, right:20, left: 60, bottom: 20};
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+    // make value accessors
+    const xValue = d => d.Population;
+    const yValue = d => d.Age_Group;
     // select all rectangles, and then pass in the data array returned
-    svg.selectAll('rect'),data(data);
+    const xScale = d3.scaleLinear()
+        .domain([0, d3.max(data, xValue)])
+        .range([0, innerWidth]);
+
+    const yScale = d3.scaleBand()
+        .domain(data.map(yValue))
+        .range([0, innerHeight])
+        .padding(0.1);
+
+    const g = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // adding in a Y axis
+    g.append("g").call(d3.axisLeft(yScale));
+    g.append("g").call(d3.axisBottom(xScale))
+        .attr("transform", `translate(0, ${innerHeight})`);
+
+    console.log(xScale.domain());
+    g.selectAll('rect').data(data)
+        .enter().append('rect')
+            .attr("y", d => yScale(yValue(d)))
+            .attr("width", d => xScale(xValue(d)))
+            .attr("height", yScale.bandwidth())
 };
 
 d3.csv("popData.csv").then(data => {
     data.forEach(d =>{
         d.Population = +d.Population * 1000;
     })
-    console.log(data);
+    render(data);
 })
--- finalProject/action.js
+++ finalProject/action.js
@@ -463,114 +463,114 @@ displayGraph("assets/Q4.csv", svg4, 200, 20)
 //     })
 
 // ------------------THIRD GRAPH------------------
-d3.csv("assets/Q3.csv", function(data) {
-    console.log(data);
-    // List of subgroups = header of the csv files = soil condition here
-    var subgroups = data.columns.slice(1)
-    console.log(subgroups);
-    // List of groups = species here = value of the first column called group -> I show them on the X axis
-    // debugger;
-    var groups = d3.map(data, function(d){
-        // debugger;
-        return(d.Group)
-    }).keys()
-    console.log(groups);
+// d3.csv("assets/Q3.csv", function(data) {
+//     console.log(data);
+//     // List of subgroups = header of the csv files = soil condition here
+//     var subgroups = data.columns.slice(1)
+//     console.log(subgroups);
+//     // List of groups = species here = value of the first column called group -> I show them on the X axis
+//     // debugger;
+//     var groups = d3.map(data, function(d){
+//         // debugger;
+//         return(d.Group)
+//     }).keys()
+//     console.log(groups);
 
-    // Add X axis
-    var x = d3.scaleBand()
-        .domain(groups)
-        .range([0, width])
-        .padding([0.3])
-    svg3.append("g")
-        .attr("transform", "translate(0," + -1 + ")")
-        .call(d3.axisTop(x).tickSizeOuter(0))
+//     // Add X axis
+//     var x = d3.scaleBand()
+//         .domain(groups)
+//         .range([0, width])
+//         .padding([0.3])
+//     svg3.append("g")
+//         .attr("transform", "translate(0," + -1 + ")")
+//         .call(d3.axisTop(x).tickSizeOuter(0))
 
-    // Add Y axis
-    var y = d3.scaleLinear()
-        .domain([100, 0])
-        .range([ height, 0 ]);
-    // svg.append("g")
-        // .call(d3.axisLeft(y));
-    var colorArray = ['#AFE1CD','#99DFF0','#E6D379', '#FBB9B9']
-    // color palette = one color per subgroup
-    var color = d3.scaleOrdinal()
-        .domain(groups)
-        .range(colorArray)
+//     // Add Y axis
+//     var y = d3.scaleLinear()
+//         .domain([100, 0])
+//         .range([ height, 0 ]);
+//     // svg.append("g")
+//         // .call(d3.axisLeft(y));
+//     var colorArray = ['#AFE1CD','#99DFF0','#E6D379', '#FBB9B9']
+//     // color palette = one color per subgroup
+//     var color = d3.scaleOrdinal()
+//         .domain(groups)
+//         .range(colorArray)
 
-    //stack the data? --> stack per subgroup
-    var stackedData = d3.stack()
-        .keys(subgroups)
-        (data)
+//     //stack the data? --> stack per subgroup
+//     var stackedData = d3.stack()
+//         .keys(subgroups)
+//         (data)
 
-    var div = d3.select("body").append("div")
-        .attr("class", "tooltip-donut")
-        .style("opacity", 0);
+//     var div = d3.select("body").append("div")
+//         .attr("class", "tooltip-donut")
+//         .style("opacity", 0);
 
-    // Show the bars
-    svg3.append("g")
-        .selectAll("g")
-        // Enter in the stack data = loop key per key = group per group
-        .data(stackedData)
-        .enter().append("g")
-        .attr("fill", function(d) { return color(d.key); })
-        .selectAll("rect")
-        // enter a second time = loop subgroup per subgroup to add all rectangles
-        .data(function(d) { return d; })
-        .enter().append("rect")
-            .attr("x", function(d) { return x(d.data.Group); })
-            .attr("y", function(d) { return y(d[0]); })
-            .attr("height", function(d) { return y(d[1])-y(d[0]); })
-            .attr("width",x.bandwidth())
-            .attr("stroke", "black")
-            .on('mouseover', function (d, i) {
-                console.log(d)
-                d3.select(this).transition()
-                     .duration('50')
-                     .attr('opacity', '.85');
-                div.transition()
-                     .duration(50)
-                     .style("opacity", 1);
-                div.html(d[1]-d[0]+ "%")
-                     .style("left", (d3.event.pageX + 10) + "px")
-                     .style("top", (d3.event.pageY - 15) + "px");
-            })
-            .on('mouseout', function (d, i) {
-                d3.select(this).transition()
-                     .duration('50')
-                     .attr('opacity', '1');
-                div.transition()
-                     .duration(50)
-                     .style("opacity", 0);
-            });
-    var legend = svg3.append('g')
-            .attr('class', 'legend')
-            .attr('transform', 'translate(' + (margin.left + 12) + ', -40)');
+//     // Show the bars
+//     svg3.append("g")
+//         .selectAll("g")
+//         // Enter in the stack data = loop key per key = group per group
+//         .data(stackedData)
+//         .enter().append("g")
+//         .attr("fill", function(d) { return color(d.key); })
+//         .selectAll("rect")
+//         // enter a second time = loop subgroup per subgroup to add all rectangles
+//         .data(function(d) { return d; })
+//         .enter().append("rect")
+//             .attr("x", function(d) { return x(d.data.Group); })
+//             .attr("y", function(d) { return y(d[0]); })
+//             .attr("height", function(d) { return y(d[1])-y(d[0]); })
+//             .attr("width",x.bandwidth())
+//             .attr("stroke", "black")
+//             .on('mouseover', function (d, i) {
+//                 console.log(d)
+//                 d3.select(this).transition()
+//                      .duration('50')
+//                      .attr('opacity', '.85');
+//                 div.transition()
+//                      .duration(50)
+//                      .style("opacity", 1);
+//                 div.html(d[1]-d[0]+ "%")
+//                      .style("left", (d3.event.pageX + 10) + "px")
+//                      .style("top", (d3.event.pageY - 15) + "px");
+//             })
+//             .on('mouseout', function (d, i) {
+//                 d3.select(this).transition()
+//                      .duration('50')
+//                      .attr('opacity', '1');
+//                 div.transition()
+//                      .duration(50)
+//                      .style("opacity", 0);
+//             });
+//     var legend = svg3.append('g')
+//             .attr('class', 'legend')
+//             .attr('transform', 'translate(' + (margin.left + 12) + ', -40)');
     
-    legend.selectAll('rect')
-        .data(subgroups)
-        .enter()
-        .append('rect')
-        .attr('x', function(d, i){
-            return (i * 200);
-        })
-        .attr('y', 0)
-        .attr('width', 12)
-        .attr('height', 12)
-        .attr('fill', function(d, i){
-            return colorArray[i];
-        });
+//     legend.selectAll('rect')
+//         .data(subgroups)
+//         .enter()
+//         .append('rect')
+//         .attr('x', function(d, i){
+//             return (i * 200);
+//         })
+//         .attr('y', 0)
+//         .attr('width', 12)
+//         .attr('height', 12)
+//         .attr('fill', function(d, i){
+//             return colorArray[i];
+//         });
     
-    legend.selectAll('text')
-        .data(subgroups)
-        .enter()
-        .append('text')
-        .text(function(d){
-            return d;
-        })
-        .attr('x', function(d, i){
-            return (i * 200) + 20;
-        })
-        .attr('y', 0)
-        .attr('text-anchor', 'start')
-        .attr('alignment-baseline', 'hanging');
-    })
+//     legend.selectAll('text')
+//         .data(subgroups)
+//         .enter()
+//         .append('text')
+//         .text(function(d){
+//             return d;
+//         })
+//         .attr('x', function(d, i){
+//             return (i * 200) + 20;
+//         })
+//         .attr('y', 0)
+//         .attr('text-anchor', 'start')
+//         .attr('alignment-baseline', 'hanging');
+//     })
