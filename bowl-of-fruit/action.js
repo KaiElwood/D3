2e4afc93d2623ce91bd6acb6c0fdb6fcac458658
KED <kaipie3@gmail.com>
2020-11-17 11:43:24 -0700

    tried to impliment modules

--- bowl-of-fruit/action.js
+++ bowl-of-fruit/action.js
@@ -3,50 +3,63 @@ function createElements(){
         .attr('height', innerHeight)
         .attr('width', innerWidth);
 
-    const height = +svg.attr('height');
-    const width = +svg.attr('width');
-    const eyeSpacing = 100;
+    const colorScale = d3.scaleOrdinal()
+        .domain(["apple", "lemon"])
+        .range(["#c11d1d", "#eae600"])
 
+    const radiusScale = d3.scaleOrdinal()
+        .domain(["apple", "lemon"])
+        .range([50, 20])
+        
+    // RENDERING LOGIC TEMPLATE
+    // references react components and structure – selection is element, props are properties
+    const render = (selection, { fruits }) => {
 
-    const circle = svg.append('circle')
-        .attr('r', 200)
-        .attr('cx', width/2)
-        .attr('cy', height/2)
-        .attr('fill', 'yellow')
-        .attr('stroke', 'black');
-
-    const leftEye = svg.append('circle')
-        .attr('r', 20)
-        .attr('cx', width/2 - eyeSpacing)
-        .attr('cy', height/2 -70)
-        .attr('fill', 'black')
-        .attr('stroke', 'black');   
-    const rightEye = svg.append('circle')
-        .attr('r', 20)
-        .attr('cx', width/2 + eyeSpacing)
-        .attr('cy', height/2 - 70)
-        .attr('fill', 'black')
-        .attr('stroke', 'black');  
-
-    const g = svg.append('g')
-        .attr('transform', `translate(${width/2}, ${height/2})`);
-
-    const nose = g.append('path')
-        .attr('d', d3.arc()({
-            innerRadius: 00,
-            outerRadius: 30,
-            startAngle: Math.PI/2,
-            endAngle: Math.PI/10
-        }))
+        // how does this work?
+        // 1. selectAll sets up elements portion of d3 data join – looks for all existing circles – makes empty selection at time code is invoked
+        // 2. then .data provides the data which NEEDS TO BE AN ARRAY
+        // 3. we now have the total number of elements and total length of array
+        // 4. enter and append will work if there are no existing dom elements for the data
+        selection.selectAll('circle').data(fruits)
+            .enter().append('circle')
+            // if we wanted to change height, we should add code below to merge section
+            .attr("cx", (d, i) => i * 150 + 100)
+            .attr("cy", innerHeight / 2)
+        .merge(selection.selectAll('circle').data(fruits))
+            .attr("r", d => radiusScale(d.type))
+            .attr("fill", d => colorScale(d.type));
+        
+        // update case – THE DATA JOIN ITSELF IS THE UPDATE SELECTION
+        // You can also use merge to avoid duplicate code –– SEE ABOVE
+        // selection.selectAll('circle').data(fruits)
+        //     .attr("r", d => radiusScale(d.type))
+        //     .attr("fill", d => colorScale(d.type));
+            
+        // exit case
+        selection.selectAll('circle').data(fruits)
+            .exit().remove();
+    }
     
-    const smile = g.append('path')
-        .attr('d', d3.arc()({
-            innerRadius: 00,
-            outerRadius: 130,
-            startAngle: Math.PI/2,
-            endAngle: Math.PI*3/2
-        }))
-        .attr('transform', `translate(0, 20)`);
+    const makeFruit = type => ({type});
+
+    const fruits = d3.range(5)
+        .map(() => makeFruit('apple'));
+
+    render(svg, { fruits });
+
+    // eating an apple
+    setTimeout(() => {
+        fruits.pop();
+        render(svg, { fruits });
+    }, 1000);
+
+    // replacing with lemon
+    // this will not work unless we use update pattern!!!
+    setTimeout(() => {
+        fruits[2].type = "lemon";
+        render(svg, { fruits });
+    }, 3000);
+
 }
 
 createElements();
